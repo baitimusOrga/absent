3b8e96093f7a80c43ab8654b5137760679424d1a
baitimus <breachmarket@gmail.com>
2025-12-01 20:58:14 +0100

    addition of home page and seperation of health page

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HealthComponent } from './health/health.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', component: HomeComponent },
+	{ path: 'health', component: HealthComponent },
+	{ path: '**', redirectTo: '' }
+];
