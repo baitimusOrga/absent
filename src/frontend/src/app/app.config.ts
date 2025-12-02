15c53e0d00ac8bd7d8a5856cc3d31be43c352d7d
baitimus <cyrus.mauch@gmx.at>
2025-12-02 11:57:39 +0100

    new header and main page

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled'
+    })),
     provideHttpClient(withFetch())
   ]
 };
--- src/frontend/src/app/home/home.component.ts
+++ src/frontend/src/app/home/home.component.ts
@@ -8,4 +8,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+export class HomeComponent {
+	readonly currentYear = new Date().getFullYear();
+}
